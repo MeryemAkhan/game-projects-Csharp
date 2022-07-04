7e6e63ff5d3a5a7fe6ae7da362a16d1a27b1bc14
Meryem Akhan <Meryem.akhn268@gmail.com>
2022-07-04 19:40:56 +0300

    maze game with trigger
    trigger with c#

---
+++ game_work_2/Assets/Scripts/Sahne1.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sahne1 : MonoBehaviour
+{
+    Rigidbody rb;
+    float guc;
+    float yatay;
+    float dikey;
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        guc = 5.0f;
+        yatay = 0.0f;
+        dikey = 0.0f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yatay = Input.GetAxis("Horizontal");
+        dikey = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(yatay * guc, 0, dikey * guc);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Cikis")
+        {
+            int SahneNo = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(SahneNo + 1);
+
+        }
+    }
+
+
+}
---
+++ game_work_2/Assets/Scripts/SonBolum.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SonBolum : MonoBehaviour
+{
+    Rigidbody rb;
+    float guc;
+    float yatay;
+    float dikey;
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        guc = 5.0f;
+        yatay = 0.0f;
+        dikey = 0.0f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yatay = Input.GetAxis("Horizontal");
+        dikey = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(yatay * guc, 0, dikey * guc);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Cikis")
+        {
+            string son = "Scenes/Sahne5";
+            SceneManager.LoadScene(son);
+            Debug.Log("TEBRİKLER");
+
+        }
+    }
+
+}
